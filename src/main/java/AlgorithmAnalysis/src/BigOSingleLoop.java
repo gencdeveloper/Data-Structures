9c1e0a6c0aa44edd9e04e9e341a9cc41fe712bdb
Emre <yunusozudogru34@gmail.com>
2024-02-29 18:41:09 +0100

    Initial commit

---
+++ src/main/java/AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -0,0 +1,13 @@
+package AlgorithmAnalysis.src;
+
+public class BigOSingleLoop {
+    public static void main(String[] args){
+        int numberOfOperations=0;
+        int n=100;
+        long startTime;
+        long endTime;
+        System.out.println("Input size n is = "+ n);
+
+        /**Task 1 Single Loop.........*/
+    }
+}
